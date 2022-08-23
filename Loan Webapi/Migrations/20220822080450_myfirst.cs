93b8b1e955536d6a7eeef8c6b83caeb035f8c52f
Tasquil <mtnoori40@gmail.com>
2022-08-23 20:07:58 +0530

    All Services changes

---
+++ Loan Webapi/Migrations/20220822080450_myfirst.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Loan_Webapi.Migrations
+{
+    public partial class myfirst : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "_loan",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    loannumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    loanamount = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    term = table.Column<int>(type: "int", nullable: false),
+                    status = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    firstname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    lastname = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__loan", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "_usr",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    username = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__usr", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "_loan");
+
+            migrationBuilder.DropTable(
+                name: "_usr");
+        }
+    }
+}
